ea4287df995e4ae850ca5ab0abbdae936ed611e1
y.faik <138728219+yfaik@users.noreply.github.com>
2024-08-10 15:01:31 +0100

    Update homepage.jsx

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -13,13 +13,13 @@ import {
 import Logo from "../components/common/logo";
 import Footer from "../components/common/footer";
 import NavBar from "../components/common/navBar";
-import Article from "../components/homepage/article";
-import Works from "../components/homepage/works";
+
+
 import AllProjects from "../components/projects/allProjects";
 
 import INFO from "../data/user";
 import SEO from "../data/seo";
-import myArticles from "../data/articles";
+
 
 import "./styles/homepage.css";
 
